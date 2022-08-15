4ee22b7be93aa9f7a785e827d3a3e95ded4e5e3d
Nadia Rozhdestvenskaia <n.rozhdestvenskaya@dodopizza.com>
2022-08-15 22:24:53 +0200

    test main cases

--- Sources/PotterKata/PotterKata.swift
+++ Sources/PotterKata/PotterKata.swift
@@ -1,23 +1,58 @@
-typealias bookTitle = Int
+typealias BookTitleId = Int
 
+typealias BooksTitles = [BookTitleId]
 
-struct PotterBookDiscountCalculation {
+struct PotterBookStore {
     let bookPrice: Double = 8
 
-    func calculateDiscount(books: [bookTitle]) -> Double {
-        let differentTitles = Set(books)
-        var discount: Double
-        switch differentTitles.count {
-        case 2: discount = 0.05
-        case ..<2: discount = 0
-        default: discount = 1
+    func calculateTotalPrice(books: BooksTitles) -> Double {
+        let discountSets = getDiscountSets(books: books)
+        var totalPrice: Double = 0
+        var booksWithoutDiscount = books.count
+        discountSets.forEach { setCount in
+            totalPrice += getPriceWithDiscount(count: setCount)
+            booksWithoutDiscount -= setCount
         }
-        return Double(differentTitles.count) * bookPrice * discount
+
+        return totalPrice + (Double(booksWithoutDiscount) * bookPrice)
+    }
+
+    private func getPriceWithDiscount(count: Int) -> Double {
+        let totalPrice = Double(count) * 8
+        let discount: Double = {
+            switch count {
+            case 2: return 0.05
+            case 3: return 0.1
+            case 4: return 0.15
+            case 5: return 0.25
+            case 6: return 0.3
+            case 7: return 0.35
+            default: return 0
+            }
+        }()
+        return (1 - discount) * totalPrice
+    }
+
+    private func getDiscountSets(books: BooksTitles) -> [Int] {
+        guard books.count > 1 else { return [] }
+        var deletableBooks = books
+        var result: [Int] = []
+        while Set(deletableBooks).count > 1 {
+            let set = Set(deletableBooks)
+            result.append(set.count)
+            deletableBooks.remove(set: set)
+        }
+        return result
     }
 }
 
+extension BooksTitles {
+    mutating func remove(_ element: BookTitleId) {
+        let index = firstIndex { $0 == element }
+        remove(at: index!)
+    }
 
-/*
-
-
- */
+    mutating func remove(set: Set<BookTitleId>) {
+        set.forEach { remove($0) }
+    }
+}
--- Tests/PotterKataTests/PotterKataTests.swift
+++ Tests/PotterKataTests/PotterKataTests.swift
@@ -2,36 +2,81 @@ import XCTest
 @testable import PotterKata
 
 final class PotterKataTests: XCTestCase {
-    var sut: PotterBookDiscountCalculation!
+    var sut: PotterBookStore!
 
     override func setUp() {
         super.setUp()
-        sut = PotterBookDiscountCalculation()
+        sut = PotterBookStore()
     }
 
-    func test_selectOneBook_getZeroDiscountPrice() {
+    func test_selectOneBook_getFullPrice() {
         let books = [1]
-        let result = 0
+        let result: Double = 8
 
-        let discount = sut.calculateDiscount(books: books)
+        let discount = sut.calculateTotalPrice(books: books)
 
         XCTAssertEqual(discount, result)
     }
 
-    func test_select1_2_2Books_get0_8DollarsDiscount(){
+    func test_select2SameBooks_getFullPrice() {
+        let books = [1, 1]
+        let result: Double = 2 * 8
+
+        let discount = sut.calculateTotalPrice(books: books)
+
+        XCTAssertEqual(discount, result)
+    }
+
+    func test_select2DifferentBooks_getPriceWithDiscount() {
+        let books = [1, 2]
+        let result: Double = 2 * 8 * 0.95
+
+        let discount = sut.calculateTotalPrice(books: books)
+
+        XCTAssertEqual(discount, result)
+    }
+
+    func test_select1_2_2Books_getPriceWithDiscount(){
         let books = [1, 2, 2]
-        let result = (8 + 8) * 0.05
+        let result: Double = (8 + 8) * 0.95 + 8
 
-        let discount = sut.calculateDiscount(books: books)
+        let discount = sut.calculateTotalPrice(books: books)
 
         XCTAssertEqual(discount, result)
     }
 
-    func test_select1_2x2_3x2Books_get0_8DollarsDiscount(){
+    func test_select1_2x2_3x2Books_getPriceWithDiscount(){
         let books = [1, 2, 2, 3, 3]
-        let result = (8 + 8) * 0.05
+        let result: Double = (8 + 8 + 8) * 0.9 + (8 + 8) * 0.95
 
-        let discount = sut.calculateDiscount(books: books)
+        let discount = sut.calculateTotalPrice(books: books)
+
+        XCTAssertEqual(discount, result)
+    }
+
+    func test_select1_2x2_3x2_4x2Books_getPriceWithDiscount(){
+        let books = [1, 2, 2, 3, 3, 4, 4]
+        let result: Double = 8 * 4 * 0.85 + 8 * 3 * 0.9
+
+        let discount = sut.calculateTotalPrice(books: books)
+
+        XCTAssertEqual(discount, result)
+    }
+
+    func test_select1_2x2_3x2_4x2_5x2_Books_getPriceWithDiscount(){
+        let books = [1, 2, 2, 3, 3, 4, 4, 5, 5]
+        let result: Double =  8 * 5 * 0.75 + 8 * 4 * 0.85
+
+        let discount = sut.calculateTotalPrice(books: books)
+
+        XCTAssertEqual(discount, result)
+    }
+
+    func test_select1_2x2_3x2_4x2_5x2_6x1Books_getPriceWithDiscount(){
+        let books = [1, 2, 2, 3, 3, 4, 4, 5, 5, 6]
+        let result: Double =  8 * 6 * 0.7 + 8 * 4 * 0.85
+
+        let discount = sut.calculateTotalPrice(books: books)
 
         XCTAssertEqual(discount, result)
     }
